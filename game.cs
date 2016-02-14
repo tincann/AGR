8636ad082bb886b246c2072788e00acd6afe4be3
tincann <saabnl@gmail.com>
2016-02-14 19:28:17 +0100

    Initial commit

---
+++ game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace Template {
+
+class Game
+{
+	public Surface screen;
+	public void Init()
+	{
+		screen.Clear( 0x2222ff );
+	}
+	public void Tick()
+	{
+		screen.Print( "hello world!", 2, 2, 0xffffff );
+	}
+	public void Render()
+	{
+		// render stuff over the backbuffer (OpenGL, sprites)
+        
+	}
+}
+
+} // namespace Template
